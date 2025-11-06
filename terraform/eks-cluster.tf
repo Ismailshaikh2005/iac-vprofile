def9e9427d87c371f0ee8797d06ee0227170038e
Ismailshaikh Irfan <ismailshaikh.irfan@localhost.localdomain>
2025-11-06 12:44:22 +0530

    EKS_Module

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -9,27 +9,26 @@ module "eks" {
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
+  # Disable actions you don't have permission for
+  create_cloudwatch_log_group = false
+  create_iam_role             = false
+
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-
       instance_types = ["t3.small"]
-
       min_size     = 1
-      max_size     = 3
+      max_size     = 2
       desired_size = 2
     }
 
     two = {
       name = "node-group-2"
-
       instance_types = ["t3.small"]
-
       min_size     = 1
       max_size     = 2
       desired_size = 1
